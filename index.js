ea7fdb462db59f33dfbb713ea40f36da5e5911c5
Nathan Miranda <nate845@gmail.com>
2018-10-25 23:16:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+let driver = {}
+
+fruit['apple'] // ('green')
+fruit.apple // ('green')
+
+// {name: 'Sam'}
+
+function updateDriverWithKeyAndValue(driver, key, value) {
+  const driverCopy = {...driver} // spread operator
+  driverCopy[key] = value
+  return driverCopy
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+  driver[key] = value
+  return driver
+}
+
+function deleteFromDriverByKey(driver, key) {
+  const driverCopy = {...driver}
+  delete driverCopy[key]
+  return driverCopy
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key) {
+  delete driver[key]
+  return driver
+}
